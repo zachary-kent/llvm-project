1da1ed3789fc4804fbff43a82e31d135237a409e
otso <55004530+OBarronCS@users.noreply.github.com>
2025-11-20 14:34:32 -0500

    fix

--- llvm_compile_opt.sh
+++ llvm_compile_opt.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+
+[ $# -eq 1 ] || { echo "need exactly one argument - path to install llvm into "; exit 1; }
+
+TARGET_DIR="$1"
+
+echo "Starting to compile and install LLVM into $TARGET_DIR"
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 pushd $SCRIPT_DIR/build
 
-# FIRST MAKE SURE TO COMMENT OUT OUR CUSTOM MACHINE PASSES
-
-TARGET_DIR="/home/otso/llvm_better"
-
 # Install our version
-# Location: /home/otso/llvm_better
 cmake -G Ninja \
   -DCMAKE_C_COMPILER=clang \
   -DCMAKE_CXX_COMPILER=clang++ \
@@ -16,7 +18,7 @@ cmake -G Ninja \
   -DCMAKE_BUILD_TYPE=Release \
   -DLLVM_USE_LINKER=lld \
   -DLLVM_PARALLEL_LINK_JOBS=1 \
-  -DCMAKE_INSTALL_PREFIX=$TARGET_DIR \
+  -DCMAKE_INSTALL_PREFIX="$TARGET_DIR" \
   -DLLVM_ENABLE_ASSERTIONS=ON \
   -DLLVM_ENABLE_PROJECTS="clang" \
   -DLLVM_TARGETS_TO_BUILD="BPF;X86" \
